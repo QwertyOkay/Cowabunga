0588bd54c8058b5400820eaea6b05b9bcfe696e4
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2023-01-02 16:27:08 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,12 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+
+  let activeAlert = false;
+    const title = "Cowabunga-tattoo.com";
+    const msg = "Победы 68/1";
+    const changeTitle = setInterval(() => {
+      document.title = activeAlert ? title : msg;
+      activeAlert = !activeAlert;
+    }, 1000);
