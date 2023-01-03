faaa6a3c11fe575a227c7ed793718070c2d3a551
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2023-01-03 15:42:50 +0200

    update

--- js/scroll.js
+++ js/scroll.js
@@ -5,4 +5,4 @@ $(window).scroll(function() {
         $('.scrolltop').stop(true, true).fadeOut();
     }
 });
-$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".header").offset().top},"1000");return false})})
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".toScroll").offset().top},"1000");return false})})
