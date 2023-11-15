b47df5c283f6a22cc78e2689514fea74d0601af5
Alexandr Kotenko <kpshmt@gmail.com>
2023-11-16 01:38:07 +0200

    upd

--- js/Pagination.js
+++ js/Pagination.js
@@ -1,11 +1,11 @@
     $(document).ready(function () {
-      var itemsPerPage = 9;
-      var $images = $('.image');
+      let itemsPerPage = 9;
+      let $images = $('.image');
 
       function showPage(page) {
         $images.hide();
-        var startIndex = (page - 1) * itemsPerPage;
-        var endIndex = startIndex + itemsPerPage;
+        let startIndex = (page - 1) * itemsPerPage;
+        let endIndex = startIndex + itemsPerPage;
         $images.slice(startIndex, endIndex).show();
 
         // Убираем класс "active" со всех кнопок пагинации
@@ -19,26 +19,10 @@
       showPage(1);
 
       $('.page-button').on('click', function () {
-        var page = parseInt($(this).data('page'));
+        let page = parseInt($(this).data('page'));
         showPage(page);
       });
     });
 
 
-// new script
-    
-// function showPage(page) {
-//   $images.hide();
-//   var startIndex = (page - 1) * itemsPerPage;
-//   var endIndex = startIndex + itemsPerPage;
-//   $images.slice(startIndex, endIndex).show();
-
-//   // Убираем класс "active" со всех кнопок пагинации
-//   $('.page-button').removeClass('active2');
-
-//   // Добавляем класс "active" только к активной кнопке
-//   $(`.page-button[data-page="${page}"]`).addClass('active2');
-
-//   // Прокручиваем секцию на 800 пикселей вверх
-//   window.scrollBy(0, -800);
-// }
+   
